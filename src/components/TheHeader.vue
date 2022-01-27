bf43e04e29f0de48449b913fcfe597a5842a6b79
Dami Kim <dami.k.dev@gmail.com>
2022-01-27 16:00:17 +0900

    feat: 상단 user menu 마크업 추가

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header :class="{ fixed: isFixed }">
     <div class="inner">
       <div
         class="open-nav-drawer"
@@ -42,14 +42,81 @@
         <div
           class="open-more"
           @click="toggleRankingWrap"></div>
+        <!-- v-if="isShowRankingWrap" -->
+        <div
+          class="ranking-wrap"
+          @click.stop="">
+          <div class="title">
+            <h3>실시간 쇼핑 검색어</h3>
+            <div class="time">
+              {{ referenceDate }} 기준
+            </div>
+            <div
+              class="close-wrap"
+              @click="toggleRankingWrap"></div>
+          </div>
+          <div class="tabs">
+            <div
+              :class="{ active: !tabIndex }"
+              class="tab"
+              @click="tabIndex = 0">
+              1~10위
+            </div>
+            <div
+              :class="{ active: tabIndex }"
+              class="tab"
+              @click="tabIndex = 1">
+              11~20위
+            </div>
+          </div>
+          <ul class="list">
+            <li
+              v-for="(rank, index) in filteredRankings"
+              :key="rank.name">
+              <a :href="rank.href">
+                <span class="index">{{ (tabIndex * 10) + index + 1 }}</span>
+                <span class="name">{{ rank.name }}</span>
+                <span class="relative-name">{{ rank.relativeName }}</span>
+              </a>
+              <div
+                :class="rank.status"
+                class="icon"></div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="user-menu">
-      </div>
+     
+      <ul class="user-menu">
+        <li class="my">
+          <a href="javascript:void(0)"></a>
+          <ul class="my__menu">
+            <li
+              v-for="item in myMenu"
+              :key="item.name">
+              <a :href="item.href">
+                {{ item.name }}
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="javascript:void(0)"></a>
+        </li>
+        <li>
+          <a href="javascript:void(0)"></a>
+        </li>
+        <li>
+          <a
+            href="javascript:void(0)"
+            @click="onNav('RNB')"></a>
+        </li>
+      </ul>
     </div>
   </header>
 </template>
 
 <script>
+import dayjs from 'dayjs'
 import Swiper from 'swiper/bundle'
 import 'swiper/css/bundle'
 
@@ -57,8 +124,30 @@ export default {
   data(){
     return{
       searchText: '',
-      rankings: {}
+      rankings: {},
+      isShowRankingWrap: false,
+      tabIndex: 0,
+      isFixed: false,
+      myMenu: [
+        { name: '나의 쿠폰', href: 'javascript:void(0)' },
+        { name: '주문/배송조회', href: 'javascript:void(0)' },
+        { name: '취소/반품/교환', href: 'javascript:void(0)' },
+        { name: '고객센터', href: 'javascript:void(0)' },
+        { name: '회원정보', href: 'javascript:void(0)' }
+      ]
     }
+  },
+  computed: {
+    referenceDate () {
+      return dayjs(this.rankings.referenceDate).format('YYYY.MM.DD HH:mm')
+    },
+    // filteredRankings () {
+    //   return this.rankings.rankings.filter((rank, index) => {
+    //     const start = this.tabIndex * 10
+    //     const end = start + 9
+    //     return start <= index && index <= end
+    //   })
+    // }
   },
   mounted(){
     this.init()
@@ -68,26 +157,40 @@ export default {
       this.rankings = await this.$fetch({
         requestName: 'rankings'
       })
-      console.log(this.rankings)
       //new Swiper(요소,옵션)
 
-      await this.$nextTick() //화면 바뀔 때까지 기다리고 스와이프 라이브러리 동작시켜라
-
-      new Swiper(this.$refs.swiper, {
-         direction: 'vertical',
-         speed: 1000,
-         autoplay: {
-           delay: 3000
-         },
-         loop: true
+      this.$nextTick(() => {
+        // https://swiperjs.com/api/
+        new Swiper(this.$refs.swiper, {
+          direction: 'vertical',
+          speed: 1000,
+          autoplay: {
+            delay: 3000
+          },
+          loop: true
+        })
       })
     },
     onNav(){
       this.$store.dispatch('navigation/onNav')
     },
-    search(){
+    async search(){
+         // 기본적인 유효성 검사
+      if (!this.searchText.trim()) return
+      const res = await this.$search({
+        searchText: this.searchText
+      })
+      console.log('검색 결과: ', res)
+      // location = res // 검색된 결과 페이지로 이동!
     },
-    toggleRankingWrap(){
+    toggleRankingWrap (event) {
+      event.stopPropagation()
+      this.isShowRankingWrap = !this.isShowRankingWrap
+      if (this.isShowRankingWrap) {
+        window.addEventListener('click', () => {
+          this.isShowRankingWrap = false
+        })
+      }
     }
   }
 }
@@ -112,7 +215,7 @@ header {
   display: flex;
   align-items: center;
 }
-  .open-nav-drawer {
+.open-nav-drawer {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -132,7 +235,8 @@ header {
     background-position: -302px -203px;
     background-size: 363px;
   }
-}.logo {
+}
+.logo {
   width: 94px;
   height: 40px;
   margin: 0 24px;
@@ -205,32 +309,209 @@ header {
     }
   }
    .open-more {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 28px;
-    height: 28px;
-    border-radius: 50%;
-    position: absolute;
-    top: 0;
-    right: 0;
-    z-index: 1; // Required! Cause Swiper.
-    cursor: pointer;
-    user-select: none; // 실시간 쇼핑 검색어가 자동으로 선택(Selection)되는 것을 방지.
-    &:hover {
-      background-color: #ececec;
-    }
-    &::after {
-      content: "";
-      display: block;
-      width: 7px;
-      height: 7px;
-      margin-top: -3px;
-      border: solid #999;
-      border-width: 0 1px 1px 0;
-      box-sizing: border-box;
-      transform: rotate(45deg);
-    }
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 28px;
+      height: 28px;
+      border-radius: 50%;
+      position: absolute;
+      top: 0;
+      right: 0;
+      z-index: 1; // Required! Cause Swiper.
+      cursor: pointer;
+      user-select: none; // 실시간 쇼핑 검색어가 자동으로 선택(Selection)되는 것을 방지.
+      &:hover {
+        background-color: #ececec;
+      }
+      &::after {
+        content: "";
+        display: block;
+        width: 7px;
+        height: 7px;
+        margin-top: -3px;
+        border: solid #999;
+        border-width: 0 1px 1px 0;
+        box-sizing: border-box;
+        transform: rotate(45deg);
+      }
    }
+   //여기부터 넣음
+      .ranking-wrap {
+        position: absolute;
+        top: 44px;
+        z-index: 2;
+        width: 410px;
+        padding: 18px 25px 25px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        box-sizing: border-box;
+        box-shadow: 0 6px 24px rgba(#000,.1);
+        background-color: #fff;
+        .title {
+          display: flex;
+          align-items: flex-end;
+          h3 {
+            font-size: 17px;
+            font-weight: 700;
+          }
+          .time {
+            margin-left: 10px;
+            font-size: 13px;
+            color: #999;
+            flex: 1;
+          }
+          .close-wrap {
+            width: 36px;
+            height: 36px;
+            background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+            background-position: -261px -203px;
+            background-size: 363px;
+            cursor: pointer;
+            margin: -6px -10px;
+            transform: scale(.6666); // from 36px to 24px
+          }
+        }
+        .tabs {
+          height: 40px;
+          margin: 18px 0;
+          border: 1px solid #eee;
+          box-sizing: border-box;
+          background-color: #fafafa;
+          display: flex;
+          .tab {
+            flex: 1;
+            color: #7d7d7d;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            &:first-child {
+              border-right: 1px solid #eee;
+            }
+            &.active {
+              background-color: #fff;
+              font-weight: 700;
+              color: #333;
+            }
+          }
+        }
+        .list {
+          li {
+            position: relative;
+            margin-top: 14px;
+            &:first-child {
+              margin-top: 0;
+            }
+            a {
+              display: block;
+              height: 22px;
+              line-height: 22px;
+              text-decoration: none;
+              font-size: 15px;
+              color: #000;
+              .index {
+                font-weight: 700;
+                margin-right: 10px;
+                color: #f43142;
+                font-style: italic;
+              }
+              .relative-name {
+                margin-left: 4px;
+                font-size: 13px;
+                color: #999;
+              }
+            }
+            .icon {
+              position: absolute;
+              top: 0;
+              right: 0;
+              width: 22px;
+              height: 22px;
+              background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+              background-size: 363px;
+              &.stay {
+                background-image: none;
+                &::after {
+                  content: "";
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                  margin: auto;
+                  width: 6px;
+                  height: 2px;
+                  background: #999;
+                }
+              }
+              &.up {
+                background-position: -110px -251px;
+              }
+              &.down {
+                background-position: -58px -251px;
+              }
+            }
+          }
+        }
+      }
+    .user-menu {
+      display: flex;
+      > li {
+        margin-right: 25px;
+        padding: 7px 0;
+        position: relative;
+        &:last-child {
+          margin-right: 0;
+        }
+        > a {
+          display: block;
+          width: 48px;
+          height: 48px;
+          background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+          background-size: 363px;
+        }
+        &:nth-child(1) > a { background-position: -106px -145px; }
+        &:nth-child(2) > a { background-position: 0px -198px; }
+        &:nth-child(3) > a { background-position: -53px -198px; }
+        &:nth-child(4) > a { background-position: -94px -70px; }
+        &:nth-child(1) > a:hover { background-position: -53px -145px; }
+        &:nth-child(2) > a:hover { background-position: -159px -145px; }
+        &:nth-child(3) > a:hover { background-position: 0px -145px; }
+        &:nth-child(4) > a:hover { background-position: -106px -198px; }
+        &.my {
+          &:hover {
+            .my__menu {
+              display: block;
+            }
+          }
+          .my__menu {
+            display: none;
+            width: 170px;
+            padding: 15px 0;
+            position: absolute;
+            top: 60px;
+            left: 0;
+            z-index: 2;
+            border: 1px solid #eee;
+            border-radius: 6px;
+            box-sizing: border-box;
+            box-shadow: 0 6px 24px -8px rgba(#000,.12);
+            background-color: #fff;
+            li {
+              a {
+                display: block;
+                padding: 7px 10px 7px 25px;
+                font-size: 15px;
+                &:hover {
+                  color: #f43142;
+                  background: #fafafa;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 }
 </style>
