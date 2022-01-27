61c72e4f4f17fc148656fc94dd834266784b2e47
Dami Kim <dami.k.dev@gmail.com>
2022-01-27 18:43:13 +0900

    feat: main.js에 lazyLoad plugin 추가

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from '~/App.vue'
 import router from '~/routes'
 import store from '~/store'
 import fetch from '~/plugins/fetch'
-
+import lazyLoad from '~/plugins/lazyLoad'
 const app = createApp(App)
 app.use(router)//플러그인 등록하는 메서드
 app.use(store)
 app.use(fetch)
+app.use(lazyLoad)
 app.mount('#app')
